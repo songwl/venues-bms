34f01c8c1255c4c2f28930127f5c38a0218025a6
songweiliang <songweiliang@wanda.cn>
2016-08-03 23:07:35 +0800

    跳转flex

---
+++ src/main/java/com/venues/bms/vo/FlexParam.java
@@ -0,0 +1,114 @@
+package com.venues.bms.vo;
+
+/**
+ * 
+ * @author song on 2016/8/3.
+ *
+ */
+public class FlexParam {
+	public static enum TP_TYPE {
+		template, page;
+	}
+
+	private String tpType;//类型，template 模板，page 页面
+	private Integer tpID;//模板id、页面id
+	private Integer isOnlyView;//是否为预览页面    0：否； 1：是
+	private Integer isNewOrModify;//是新增还是编辑操作   0:新增 ;1:编辑
+	private Integer pageTypeID;//页面类型
+	private Integer userId;//当前用户id
+	private String userType;//当前用户类型
+	private Integer userTypeParentId;//当前用户类型的父ID
+
+	public String getTpType() {
+		return tpType;
+	}
+
+	public void setTpType(String tpType) {
+		this.tpType = tpType;
+	}
+
+	public Integer getTpID() {
+		return tpID;
+	}
+
+	public void setTpID(Integer tpID) {
+		this.tpID = tpID;
+	}
+
+	public Integer getIsOnlyView() {
+		return isOnlyView;
+	}
+
+	public void setIsOnlyView(Integer isOnlyView) {
+		this.isOnlyView = isOnlyView;
+	}
+
+	public Integer getIsNewOrModify() {
+		return isNewOrModify;
+	}
+
+	public void setIsNewOrModify(Integer isNewOrModify) {
+		this.isNewOrModify = isNewOrModify;
+	}
+
+	public Integer getPageTypeID() {
+		return pageTypeID;
+	}
+
+	public void setPageTypeID(Integer pageTypeID) {
+		this.pageTypeID = pageTypeID;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public Integer getUserTypeParentId() {
+		return userTypeParentId;
+	}
+
+	public void setUserTypeParentId(Integer userTypeParentId) {
+		this.userTypeParentId = userTypeParentId;
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("_o=" + super.toString());
+		if (this.tpType != null) {
+			sb.append("&tpType=" + this.tpType);
+		}
+		if (this.tpID != null) {
+			sb.append("&tpID=" + this.tpID);
+		}
+		if (this.isOnlyView != null) {
+			sb.append("&isOnlyView=" + this.isOnlyView);
+		}
+		if (this.isNewOrModify != null) {
+			sb.append("&isNewOrModify=" + this.isNewOrModify);
+		}
+		if (this.pageTypeID != null) {
+			sb.append("&pageTypeID=" + this.pageTypeID);
+		}
+		if (this.userId != null) {
+			sb.append("&userId=" + this.userId);
+		}
+		if (this.userType != null) {
+			sb.append("&userType=" + this.userType);
+		}
+		return super.toString();
+	}
+
+}
---
+++ src/main/java/com/venues/bms/web/page/PageAuditController.java
@@ -0,0 +1,79 @@
+package com.venues.bms.web.page;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.math.NumberUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.venues.bms.core.model.Page;
+import com.venues.bms.core.model.ResultMessage;
+import com.venues.bms.po.PgPage;
+import com.venues.bms.service.page.PageService;
+import com.venues.bms.service.sys.LogService;
+import com.venues.bms.vo.Enums;
+import com.venues.bms.web.BaseController;
+
+/**
+ * 
+ * Created by song on 2016/7/25.
+ */
+@Controller
+@RequestMapping(value = "/page/audit")
+public class PageAuditController extends BaseController {
+
+	@Autowired
+	private PageService pageService;
+
+	@Autowired
+	private LogService logService;
+
+	@RequestMapping(value = "/list")
+	public String list(ModelMap model) throws Exception {
+		Page<PgPage> page = this.getPageRequest();
+		Map<String, Object> params = this.getSearchRequest();
+
+		List<String> orderBy = new ArrayList<>();
+		orderBy.add("id desc");
+		page.setOrderBy(orderBy);
+		page = pageService.findPgPages(page, params);
+		model.put("page", page);
+		model.put("searchParams", params);
+		return "pg/audit_list";
+	}
+
+	@RequestMapping(value = "/batchPass", method = RequestMethod.GET)
+	@ResponseBody
+	public ResultMessage batchPass(String ids) {
+		String[] arr = ids.split(",");
+		for (String str : arr) {
+			int id = NumberUtils.toInt(str);
+			if (id > 0) {
+				//pageService.updatePassFile(id);
+			}
+		}
+		logService.saveLog(Enums.LOG_TYPE.UPDATE, this.getCurrentAccount().getLoginUsername(), "页面审核", "批量页面审核通过：资源ID=" + ids);
+		return this.ajaxDoneSuccess("批量审核成功");
+	}
+
+	@RequestMapping(value = "/batchUnPass", method = RequestMethod.GET)
+	@ResponseBody
+	public ResultMessage batchUnPass(String ids) {
+		String[] arr = ids.split(",");
+		for (String str : arr) {
+			int id = NumberUtils.toInt(str);
+			if (id > 0) {
+				//pageService.updatePassFile(id);
+			}
+		}
+		logService.saveLog(Enums.LOG_TYPE.UPDATE, this.getCurrentAccount().getLoginUsername(), "页面审核", "批量页面审核不通过：页面ID=" + ids);
+		return this.ajaxDoneSuccess("批量审核成功");
+	}
+
+}
